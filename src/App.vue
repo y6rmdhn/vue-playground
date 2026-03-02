a6c9fff845f138aa0543f56c45b1aed30ede16c6
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2026-03-02 14:14:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header> -->
+
+  <main>
+    <h1>Hello World!</h1>
+    <p>Maghfira</p>
+  </main>
+</template>
+
+<style scoped></style>
